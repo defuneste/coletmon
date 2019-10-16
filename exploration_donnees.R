988f62a62f0d549bfc7fe515a2efdd75b920acac
defuneste <leroy.oli@gmail.com>
2019-10-16 21:05:55 +0200

    création fichier exploration des données + chargement

---
+++ exploration_donnees.R
@@ -0,0 +1,24 @@
+# script d'exploration des données pour le projet col&mon
+# 16-10-2019
+##.###################################################################################33
+## I. Chargement des données de l'xp ====
+##.#################################################################################33
+
+## 1 - Les bibliotheques ================
+
+library(sp) # ancien package spatial toujours présent
+library(sf) # package spatial
+library(dplyr) # manip données
+library(lubridate) # les dates 
+library(forcats) # pour les facteurs
+
+
+## 2 - Les données ================
+
+#celui ci est un tible
+fait.dat <- readRDS("data/T0impl.Rds")
+class(fait.dat)
+
+#celui ci est un df
+implantation.dat <- readRDS("data/T0New.Rds")
+class(implantation.dat)
