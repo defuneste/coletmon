24cc1d4a6c3127859dce77fb21030ec5efdc7ab4
defuneste <leroy.oli@gmail.com>
2020-01-16 12:40:40 +0100

    degrecum et tjrs pb implantation

---
+++ degrecum.R
@@ -0,0 +1,38 @@
+##### DegreCum
+
+
+# 1 - chargement des rds =================
+T0relation <- readRDS("data/T0relation.rds")
+# on vire les deplacements
+T0relation <- T0relation[!T0relation$modaNiv1 == "Déplacement",]
+
+# la fonction prend un niveau hierarchique par defaut
+# relation correspond à un T0relation
+# on a besoin d'igraph
+
+degreCum <- function(relation, niveau_hierarchique = 1) {
+    if(require("igraph") == FALSE)  
+        install.packages("igraph",  dependencies=c("Depends", "Suggests"))
+    if(require("sf") == FALSE)  
+        install.packages("igraph",  dependencies=c("Depends", "Suggests"))
+    # on drop la geometry
+    vertex <- st_drop_geometry(relation)
+    # ici c'est un subest pour alléger et virer des NA, il ne devrait plus y en avoir mais bon ...
+    relation_graph <- subset(vertex, fklinked_implantation != "NA",  select = c(idimplantation, fklinked_implantation))
+    # on fait le graph et on le siplifie
+    graph_relation <- graph.data.frame(relation_graph, directed = TRUE)
+    graph_modaNiv1_simplify <- simplify(graph_relation) # mal absolu car par defaut ne garde que la première ligne du doublon
+    # attention fonction à partir de i graph 1.0
+    voisin <- make_ego_graph(graph_modaNiv1_simplify, order = niveau_hierarchique, mode = "out") 
+    idimplantation <- V(graph_modaNiv1_simplify)$name
+    degre <- sapply(voisin, vcount)
+    data.frame(idimplantation, degre)
+}
+
+# 2 - pour tester ================= 
+
+unique(T0relation$modaNiv1)
+
+T0relation <- T0relation[T0relation$modaNiv1 == "hiérarchique asc. Ecole",]
+
+head(degreCum(T0relation, 3), 20)
--- graph_anaviz.R
+++ graph_anaviz.R
@@ -4,67 +4,71 @@
 
 library(igraph)
 library(threejs)
+library(sf)
 
 T0relation <- readRDS("data/T0relation.rds")
 
-is.simple(bob)
+implantation.dat <- readRDS("data/T0impl20191126.rds")
 
+# je vire les deplacements car cela pose pb 
+# T0relation[ ! T0relation$idimplantation %in% T0relation$fklinked_implantation,   ]
+# unique(T0relation$modaNiv1)
 
+T0relation <- T0relation[!T0relation$modaNiv1 == "Déplacement",]
+T0relation$role[is.na(T0relation$role)] <- "Ecole"
+
+table(T0relation$role, T0relation$modaNiv1)
+
+implantation <- T0relation
+
+T0relation <- T0relation[T0relation$modaNiv1 == "Relation horizontale",]
+
+niveau_hierarchique <- 1 # 
 
 vertex <- st_drop_geometry(T0relation)
-
 
 #vertex <- vertex[vertex$modaNiv1 == "hiérarchique descendante",]
 
 relation_graph <- subset(vertex , fklinked_implantation != "NA",  #il y a une valeur manquante
                          select = c(idimplantation, fklinked_implantation))
 
-length(unique(relation_graph$idimplantation))
-
-length(unique(relation_graph$fklinked_implantation))
-
-
-vertex_v1 <-  vertex[match(unique(c(relation_graph$idimplantation, relation_graph$fklinked_implantation)), 
-             vertex$idimplantation),]
-
-dim(vertex_v1)
+# length(unique(relation_graph$idimplantation))
+# 
+# length(unique(relation_graph$fklinked_implantation))
 
 # il me faut une option pour dessiner les vertex à partir des relations 
-length(unique(c(relation_graph$idimplantation, relation_graph$fklinked_implantation)))
+# length(unique(c(relation_graph$idimplantation, relation_graph$fklinked_implantation)))
+# ou alors il me faut un moyen pour reconstituer les vertexes à prtir de la sélection
+
+vertex_v1 <-  implantation[match(unique(c(relation_graph$idimplantation, relation_graph$fklinked_implantation)), 
+             implantation$idimplantation),]
 
 
-sum( ! relation_graph$fklinked_implantation %in% relation_graph$idimplantation )
-
-sum(xor(relation_graph$fklinked_implantation, relation_graph$idimplantation))
-
-T0relation[T0relation$idimplantation == 3095,]
+# dim(vertex_v1)
 
 graph_relation <- graph.data.frame(relation_graph, 
-                                   directed = FALSE,
+                                   directed = TRUE, # ici on est dans un graph dirigé
                                    vertices = vertex_v1 )
+V(graph_relation)
+V(graph_relation)$role
 
-graph_relation <- graph.data.frame(relation_graph, 
-                                   directed = FALSE)
+graph_modaNiv1_simplify <- simplify(graph_relation) # mal absolu 
 
-is.simple(graph_relation)
+niveau_hierarchique <- 3 # 
+un.voisin <- make_ego_graph(graph_modaNiv1_simplify, order = niveau_hierarchique, mode = "out") # attention la fonction evolue vers ego_size
+sapply(un.voisin, vcount)
 
-
-graph_modaNiv1_simplify <- simplify(graph_relation) # mal absolu
-
-
-graph_relation <- bob
-
-unique(T0relation$role)
-
-V(graph_modaNiv1_simplify)$colorV <- ifelse(V(graph_modaNiv1_simplify)$role == "Dominant", "blue",
+V(graph_modaNiv1_simplify)$colorV <- ifelse(V(graph_modaNiv1_simplify)$role == "Dominant" | V(graph_modaNiv1_simplify)$role == "Dominant_ecole", "blue",
                                   ifelse(V(graph_modaNiv1_simplify)$role == "Dominé", "red", 
                                          ifelse(V(graph_modaNiv1_simplify)$role == "Égal", "forestgreen", "black")))
 
-graphjs(graph_relation,
-        vertex.label = paste(V(graph_relation)$usual_name, V(graph_relation)$name), # il faut usual name
+V(graph_modaNiv1_simplify)$shape <- ifelse(V(graph_modaNiv1_simplify)$role == "Ecole" , "square", "circle")
+
+graphjs(graph_modaNiv1_simplify,
+        vertex.label = paste(V(graph_modaNiv1_simplify)$usual_name, V(graph_modaNiv1_simplify)$name), # il faut usual name
         vertex.color = V(graph_modaNiv1_simplify)$colorV,
-        vertex.size = 0.2, # pe modifier par degree
-        vertex.shape = "square",
+        vertex.size = log(sapply(un.voisin, vcount))/5,
+        vertex.shape = V(graph_modaNiv1_simplify)$shape,
         #edge.color = E(un_sous_graph)$colorW, 
         brush=TRUE)
 
@@ -97,3 +101,9 @@ mise_enplace_graph <- function(relation, implantation = NULL ) {
                                        directed = FALSE,
                                        vertices = implantation_vertex )
 }
+
+mise_enplace_graph(T0relation, implantation.dat)
+
+
+
+
