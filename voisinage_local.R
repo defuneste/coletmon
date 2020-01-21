0778adfa48998f02e727bc9740c9bf805f5182ab
defuneste <leroy.oli@gmail.com>
2020-01-21 10:36:16 +0100

    voisinage_local_opt2

--- voisinage_local.R
+++ voisinage_local.R
@@ -1,4 +1,9 @@
 # fonction voisinage local
+library(dplyr)
+library(sf)
+library(igraph)
+library(threejs)
+
 
 # 1 - chargement des rds =================
 T0relation <- readRDS("data/T0relation.rds")
@@ -56,22 +61,27 @@ return(data.frame(idimplantation, niveau))
 
 }
 
-voisinage_local_opt1(T0relation, 99, 2)
-
 #pour tester
 relation <- T0relation
 idimplantation_saisie <- 99
 niveau <- 15
 
-#### bordel ===============================
+voisinage_local_opt1(T0relation, 99, 2)
 
+#### voisinage_local_opt2 ===============================
+# cette fonction produit pour un TOrelation et une idimplantation saisie un tableau avec :
+# idimplantation et niveau, ici niveau correspond au niveau le plus cours connectant à l'idimplantation saisie
+# cette fonction nécessite graph_a_partir_id() et voisinage_local_opt1()
+# elle utilise igraph et dplyr
 
 idimplantation_saisie <- 99
 relation <- T0relation
 
-voisinage_local_opt1(T0relation, 99, 2)
+voisinage_local_opt2 <- function(relation, idimplantation_saisie) {
 
-
+## ici j'ai besoin du graph de la composantes connexes pour obtenir le diamètre
+# c'est un peu redondant avec voisinage_local_opt1
+    
 relation_graph <- relation %>% 
   st_drop_geometry() %>% 
   filter( !is.na(fklinked_implantation)) %>%  # au cas ou on a des NA
@@ -81,46 +91,30 @@ relation_graph <- relation %>%
 graph_relation <- simplify(graph.data.frame(relation_graph, 
                                             # ici si on est en "Relation horizontale" on est dans un graph non dirigé sinon dirigé
                                             directed = FALSE)) # à noter on est en non dirigé
-
+# on peut gagner en objet intermediaire en passant directement graph_relation dans graph_a_partir_id
+# mais on perds en lisbilité
 sous_graph <- graph_a_partir_id(idimplantation_saisie, graph_relation)
 
+# on initialise la list
+une_list <- list()
 
-length(as_ids(get_diameter(sous_graph)))
-
-
+# chaque df par niveau va aller dans une_list 
 for(i in 1:length(as_ids(get_diameter(sous_graph)))) {
+  une_itération <- voisinage_local_opt1(relation, idimplantation_saisie, i)
+  une_list[[i]] <- une_itération 
+  }
 
-test <- bind_rows(bob, bill)   %>% bind_rows(jim) %>% distinct(test, .keep_all = TRUE)
-
+# on va retourner
+# on ajoute les differents tableaux
+return(do.call(rbind, une_list) %>%
+           # on filtre en ne gardant que la première valeur d'idimplantation
+            dplyr::distinct(idimplantation, .keep_all = TRUE))
 }
 
 
+#### pour tester 
 
-graphjs(un_sous_graph,
-        vertex.label = paste(V(un_sous_graph)$usual_name, V(un_sous_graph)$name), # il faut usual name
-        vertex.size = 0.2, # pe modifier par degree
-        brush=TRUE)
+voisinage_local_opt2(T0relation, 99)
 
-
-
-degreCum_modaNiv1 <- function(relation, niveau_hierarchique = 10000, ValmodaNiv1) {
-    # on drop la geometry
-    relation <- st_drop_geometry(relation) %>%
-        filter( !is.na(fklinked_implantation) & modaNiv1 == ValmodaNiv1) %>%
-        select(idimplantation, fklinked_implantation)
-    # on fait le graph et on le siplifie
-    graph_relation <- simplify(graph.data.frame(relation, 
-                                                # ici si on est en "Relation horizontale" on est dans un graph non dirigé sinon dirigé
-                                                directed = ifelse(ValmodaNiv1 == "Relation horizontale", FALSE, TRUE))) 
-    # simplify=mal absolu car par defaut ne garde que la première ligne du doublon
-    
-    # attention fonction à partir de igraph 1.0
-    voisin <- make_ego_graph(graph_relation, order = niveau_hierarchique, mode = "out")
-    # un vecteur des idimplantation
-    idimplantation <- V(graph_relation)$name
-    # la somme des degrés pour chaque id pour pleins de voisins, ici 10000
-    degreCum <- sapply(voisin, vcount)
-    # on a besoin de modaNiv1
-    modaNiv1 <- rep(ValmodaNiv1, length(idimplantation))
-    return(data.frame(idimplantation, modaNiv1, degreCum))
-}
+# le diamétre est de 15 mais avec 8 niveau on arrive à avoir toutes les implantations
+table(voisinage_local_opt2(T0relation, 99)$niveau)
