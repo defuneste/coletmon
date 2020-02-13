87ab40b5b3bb60be609d5bd85ffb1c984ced9eba
defuneste <leroy.oli@gmail.com>
2020-02-13 17:44:56 +0100

    debut anim pb avec multiples

--- animation_cluny.R
+++ animation_cluny.R
@@ -1,8 +1,7 @@
 library(dplyr)
 library(igraph)
+library(intergraph)
 
-library("networkDynamic") # attention masque un paquet de truc de igraph
-library(ndtv)
 
 #
 
@@ -31,28 +30,65 @@ T0relation <- T0relation[!is.na(T0relation$date_stopC),]
 
 relation <- subset(T0relation, !(T0relation$modaNiv1 == "hiérarchique asc. Ecole" | T0relation$modaNiv1 == "hiérarchique ascendante") ) %>% 
   # attention c'est important d'avoir les deux premières colonnes qui indiquent les liens A ---- B 
-  dplyr::select(idimplantation, fklinked_implantation, usual_name, linked_implantation_name, modaNiv1, date_startC, date_stopC, idfactoid) 
+  dplyr::select(tail = idimplantation, head = fklinked_implantation,  onset = date_startC, terminus = date_stopC, modaNiv1, usual_name, linked_implantation_name) 
 
 
 partA <- relation %>% 
-  select(idimplantation, usual_name)
+  select(tail, usual_name)
 
 idimpl_nom <- relation %>% 
-  select(idimplantation = fklinked_implantation, usual_name = linked_implantation_name) %>% 
+  select(tail = head, usual_name = linked_implantation_name) %>% 
   bind_rows(partA) %>% 
-  distinct(idimplantation, .keep_all = TRUE)
+  distinct(tail, .keep_all = TRUE)
 
 rm(partA)
 
-vertex_v1 <-  idimpl_nom[match(unique(c(relation$idimplantation, relation$fklinked_implantation)), 
-                               idimpl_nom$idimplantation),]
+vertex_v1 <-  idimpl_nom[match(unique(c(relation$tail, relation$head)), 
+                               idimpl_nom$tail),]
+
+graph_relation <- graph.data.frame(relation, directed = FALSE, vertices = vertex_v1)
+comps <- decompose.graph(graph_relation)
+
+count_multiple(graph_relation)
+relation[which_multiple(graph_relation),] %>% 
+        arrange(tail)
+
+relation$multiple<- count_multiple(graph_relation)
+
+relation_multiple <- relation[relation$multiple > 1,] %>% 
+    arrange(tail)
+openxlsx::write.xlsx(file = "relation_multiple.xlsx", relation_multiple)
+
+
+sapply(comps, vcount)
+
+cluny <- comps[[3]]
+    
+E(cluny)$onset[which_multiple(cluny)]
+
+cluny_net <- asNetwork(cluny)
+
+library(networkDynamic) # attention masque un paquet de truc de igraph
+library(ndtv)
+
+clunny_net %e% "date_startC"
+
+vs <- data.frame(onset=0, terminus=50, vertex.id=1:17)
+es <- data.frame(onset=1:49, terminus=50, 
+                 head=as.matrix(net3, matrix.type="edgelist")[,1],
+                 tail=as.matrix(net3, matrix.type="edgelist")[,2])
+
+cluny_dyn <- networkDynamic(cluny_net)
+
+
+render.d3movie(cluny_dyn, usearrows = F, displaylabels = F)
+
 
 
 
 # les packages pour faire des graphs d'animation 
 
-library("networkDynamic") # attention masque un paquet de truc de igraph
-library(ndtv)
+
 
 
 hc.spls <- cbind((hc$Time-20)/(60*60),  hc$Time/(60*60), hc$ID1, hc$ID2)
