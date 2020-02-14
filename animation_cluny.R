774132e55942fc0a5dc58176400e4ba23503a3ba
defuneste <leroy.oli@gmail.com>
2020-02-14 17:41:33 +0100

    animation debut

--- animation_cluny.R
+++ animation_cluny.R
@@ -30,7 +30,9 @@ T0relation <- T0relation[!is.na(T0relation$date_stopC),]
 
 relation <- subset(T0relation, !(T0relation$modaNiv1 == "hiérarchique asc. Ecole" | T0relation$modaNiv1 == "hiérarchique ascendante") ) %>% 
   # attention c'est important d'avoir les deux premières colonnes qui indiquent les liens A ---- B 
-  dplyr::select(tail = idimplantation, head = fklinked_implantation,  onset = date_startC, terminus = date_stopC, modaNiv1, usual_name, linked_implantation_name) 
+  dplyr::select(tail = idimplantation, head = fklinked_implantation,  onset = date_startC, terminus = date_stopC, modaNiv1, usual_name, linked_implantation_name) %>% 
+  dplyr::mutate(dure = terminus - onset) %>% 
+  dplyr::filter(dure >= 0)    
 
 
 partA <- relation %>% 
@@ -77,15 +79,21 @@ cluny_net <- asNetwork(cluny)
 library(networkDynamic) # attention masque un paquet de truc de igraph
 library(ndtv)
 
-clunny_net %e% "date_startC"
 
-vs <- data.frame(onset=0, terminus=50, vertex.id=1:17)
-es <- data.frame(onset=1:49, terminus=50, 
-                 head=as.matrix(net3, matrix.type="edgelist")[,1],
-                 tail=as.matrix(net3, matrix.type="edgelist")[,2])
+# vertex
+vs <- data.frame(onset = 0, terminus = 1792, vertex.id=1:length(cluny_net %v% "vertex.names"), name = cluny_net %v% "vertex.names")
+dim(vs)
 
-cluny_dyn <- networkDynamic(cluny_net)
+# edge spells
+es <- data.frame(onset = cluny_net %e% "onset",
+                 terminus = cluny_net %e% "terminus",
+                 head = as.matrix(cluny_net, matrix.type="edgelist")[,1],
+                 tail = as.matrix(cluny_net, matrix.type="edgelist")[,2])
+dim(es)
 
+cluny_dyn <- networkDynamic(cluny_net, edge.spells = es, vertex.spells = vs)
+
+compute.animation(slice.par=list(start=0, end=1792, interval=100, aggregate.dur=100, rule='any'))
 
 render.d3movie(cluny_dyn, usearrows = F, displaylabels = F)
 
